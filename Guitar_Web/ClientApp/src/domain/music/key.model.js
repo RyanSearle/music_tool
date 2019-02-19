5881c12026f24962102833296df4cf2a71d86fe5
Ryan <Ryansearle96@gmail.com>
2019-02-19 14:18:54 +0000

    Initial work on scale rework

--- Guitar_Web/ClientApp/src/domain/music/key.model.js
+++ Guitar_Web/ClientApp/src/domain/music/key.model.js
@@ -80,3 +80,104 @@ export const Key = (function () {
 
     return Key;
 })();
+
+
+export const Tone = (function (){
+
+    // pitch represents the 0-11 value of the chromatic scale
+    const Tone = function(pitch, octave) {
+        this.pitch = pitch;
+        this.octave = octave;
+    }
+
+    Tone.prototype.getKey = function (targetLetter, scaleResolver) {
+        return new Key(this.pitch, targetLetter, scaleResolver);
+    }
+
+    return Tone;
+})();
+
+export const Key = (function (){
+
+    const rationalScale = [
+        { char: 'A', value: 0 },
+        { char: 'B', value: 2 },
+        { char: 'C', value: 3 },
+        { char: 'D', value: 5 },
+        { char: 'E', value: 7 },
+        { char: 'F', value: 8 },
+        { char: 'G', value: 10 },
+    ]
+
+    const chromaticScale = [
+        { char: 'A', value: 0 },
+        { char: 'A#', value: 1 },
+        { char: 'B', value: 2 },
+        { char: 'C', value: 3 },
+        { char: 'C#', value: 4 },
+        { char: 'D', value: 5 },
+        { char: 'D#', value: 6 },
+        { char: 'E', value: 7 },
+        { char: 'F', value: 8 },
+        { char: 'F#', value: 9 },
+        { char: 'G', value: 10 },
+        { char: 'G#', value: 11 }
+    ]
+
+    const Key = function(pitch, targetLetter, scaleResolver) {
+
+        // If no target letter is provided then use the default chromatic scale
+        if(!targetLetter) {
+            targetLetter = chromaticScale.find(x => x.value === pitch).char;
+        }
+
+        this.letter = targetLetter;
+
+        const letter = rationalScale.find(x => x.char === targetLetter);
+
+        if(!letter) throw new Error('targetLetter not found');
+
+        switch (scaleResolver) {
+            case ScaleResolver.SHARPEN:
+                this.modifier = resolveWithSharps(letter.value, pitch);
+                break;
+            case ScaleResolver.FLATTEN:
+                this.modifier = resolveWithFlats(letter.value, pitch);
+                break;
+            default:
+                throw new Error('scaleResolver Not Found');
+        }
+    }
+
+    Key.prototype.getDisplayCharacter = function() {
+        const modifierChar = this.modifier >= 0 ? '#' : '♭';
+        let toAdd = '';
+        for (let index = 0; index < this.modifier; index++) {
+            toAdd = toAdd + modifierChar;
+        }
+
+        return `${this.letter}${toAdd}`;
+    }
+
+    const resolveWithSharps = function(current, target) {
+        if(current === target) return 0;
+        if(current > target) {
+            current = current - 12;
+        }
+        return target - current;
+    }
+
+    const resolveWithFlats = function(current, target) {
+        if(current === target) return 0;
+        if(current < target) {
+            current = current + 12;
+        }
+        return target - current;
+    }
+})();
+
+
+export const ScaleResolver = {
+    SHARPEN: 1,
+    FLATTEN: 2
+}
